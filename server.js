7f1c6f19abc29bbd0a41cb54627509f85725156c
spenserg92 <spenserg92@gmail.com>
2023-12-21 17:13:28 -0500

    adds connection and middleware functions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express') // import express framework
 require('dotenv').config() // import/load ENV variables
 const path = require('path') // import path module
-
+const middleware = require('./utils/middleware')
 
 
 /// Import Routers ///
@@ -13,12 +13,13 @@ const path = require('path') // import path module
 /// Create App Object ///
 const app = express() // Call the express function
 
-
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 /// Middleware ///
 
 
-
+middleware(app)
 
 /// Routes ///
 // basic home route
---
+++ utils/middleware.js
@@ -0,0 +1,33 @@
+/// Import Dependencies ///
+const express = require('express')
+const morgan = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+require('dotenv').config()
+const methodOverride = require('method-override')
+
+
+
+/// Middleware Function ///
+
+const middleware = (app) => {
+    app.use(methodOverride('_method'))
+    app.use(morgan('tiny'))
+    app.use(express.static('public'))
+    app.use(express.json())
+
+    app.use(
+        session({
+            secret: process.env.SECRET,
+            store: MongoStore.create({
+                mongoUrl: process.env.DATABASE_URL
+            }),
+            saveUninitialized: true,
+            resave: false
+        })
+    )
+}
+
+/// Export the middleware function ///
+module.exports = middleware
+
