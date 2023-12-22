1357a9efed78f441d3e4420f79c2d43109f613d2
spenserg92 <spenserg92@gmail.com>
2023-12-22 13:12:44 -0500

    adds user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+/// Schema and Dependencies ///
+
+const mongoose = require('../utils/connection')
+
+const {Schema, model} = mongoose
+
+/// Schema Definition ///
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+/// Create User Model ///
+
+const User = model('User', userSchema)
+
+
+/// Export User Model ///
+
+module.exports = User
