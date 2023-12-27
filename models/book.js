d82e70ddb1dc886df1959c638aa7c03143017d0f
spenserg92 <spenserg92@gmail.com>
2023-12-27 15:21:27 -0500

    adds book and review models and schemas

---
+++ models/book.js
@@ -0,0 +1,31 @@
+const mongoose = require('../utils/connection')
+
+const { Schema, model } = mongoose
+
+const bookSchema = {
+    title: {
+        type: String,
+        required: true,
+    },
+    genre: { type: String },
+    author: {
+        type: String,
+        required: true,
+    },
+    publishedDate: { type: Date },
+    imgCover: { type: String },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}
+
+/// Create User Model ///
+
+const Book = model('Book', bookSchema)
+
+
+/// Export User Model ///
+
+module.exports = Book
---
+++ models/review.js
@@ -0,0 +1,36 @@
+/// Schema and Dependencies ///
+
+const mongoose = require('../utils/connection')
+
+const {Schema, model} = mongoose
+
+const reviewSchema = {
+    reviewer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    book: {type: Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true},
+    review: {
+        type: String,
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    hasRead: {
+        type: Boolean,
+    }
+}
+
+
+
+const Review = model('Review', reviewSchema)
+
+
+/// Export User Model ///
+
+module.exports = Review
